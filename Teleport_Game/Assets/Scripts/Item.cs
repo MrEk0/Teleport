bb8da54d6ec6df1698e588c46427a4a6470dd1e0
MrEk0 <51297787+MrEk0@users.noreply.github.com>
2019-10-07 22:00:12 +0300

    item's rotation

---
+++ Teleport_Game/Assets/Scripts/Item.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Item : MonoBehaviour
+{
+    [SerializeField] float rotationSpeed = 10f;
+    [SerializeField] float speed = 10f;
+
+    Rigidbody2D rd;
+    Transform myTransform;
+
+    private void Awake()
+    {
+        rd = GetComponent<Rigidbody2D>();
+        myTransform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myTransform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
+    }
+
+    private void FixedUpdate()
+    {
+        rd.velocity = Vector2.down*speed;
+    }
+}
