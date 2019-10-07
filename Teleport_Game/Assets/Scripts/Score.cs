6cdb9718f83f2857a4a9726c04f9b9e108c6932e
MrEk0 <51297787+MrEk0@users.noreply.github.com>
2019-10-07 21:52:19 +0300

    score system

---
+++ Teleport_Game/Assets/Scripts/Score.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    private Text scoreText;
+    Player player;
+
+    private float startScore = 0f;
+
+    private void Awake()
+    {
+        player = FindObjectOfType<Player>();
+        scoreText = GetComponent<Text>();
+    }
+
+    private void OnEnable()
+    {
+        player.OnGetBanana += ChangeScore;
+    }
+
+    private void OnDisable()
+    {
+        player.OnGetBanana -= ChangeScore;
+    }
+
+    private void Start()
+    {
+        scoreText.text = startScore.ToString();
+    }
+
+    private void ChangeScore(float additionalScore)
+    {
+        startScore += additionalScore;
+        scoreText.text = startScore.ToString();
+    }
+}
