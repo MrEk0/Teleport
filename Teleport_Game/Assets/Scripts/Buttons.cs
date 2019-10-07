438530cf29698e8df32460cd4ef3da91a77833c4
MrEk0 <51297787+MrEk0@users.noreply.github.com>
2019-10-07 22:02:34 +0300

    buttons

---
+++ Teleport_Game/Assets/Scripts/Buttons.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Buttons : MonoBehaviour
+{
+    Canvas canvas;
+
+    private void Awake()
+    {
+        canvas = FindObjectOfType<Canvas>();
+    }
+
+    private void Start()
+    {
+        Time.timeScale = 1f;
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
