db680a2171d68648aab23a536db97f57207ecadb
MrEk0 <51297787+MrEk0@users.noreply.github.com>
2019-10-07 21:58:32 +0300

    lose life

---
+++ Teleport_Game/Assets/Scripts/Life.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Life : MonoBehaviour
+{
+    [SerializeField] GameObject losePanel = null;
+
+    private Image[] lifes;
+    Player player;
+    Canvas canvas;
+
+    private int lifeCount = 0;
+
+    private void Awake()
+    {
+        player = FindObjectOfType<Player>();
+        canvas = GetComponentInParent<Canvas>();
+
+        lifes = GetComponentsInChildren<Image>();
+        lifeCount = lifes.Length;
+    }
+
+    private void OnEnable()
+    {
+        player.OnTouchBomb += LoseLife;
+    }
+
+    private void OnDisable()
+    {
+        player.OnTouchBomb -= LoseLife;
+    }
+
+    private void LoseLife()
+    {
+        lifeCount--;
+
+        if (lifeCount > 0)
+        {
+            lifes[lifeCount].enabled = false;
+        }
+        else
+        {
+            ShowLosePanel();
+        }
+    }
+
+    private void ShowLosePanel()
+    {
+        Time.timeScale = 0f;
+        Instantiate(losePanel, canvas.transform);
+    }
+}
